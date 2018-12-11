a63ca5415daa8380eb232e9fc2e50b9c9b92b017
OCTO <octo@octo.com>
2018-12-11 16:08:23 +0100

    fin tp8

--- tp-ansible/roles/haproxy/molecule/default/tests/test_default.py
+++ tp-ansible/roles/haproxy/molecule/default/tests/test_default.py
@@ -11,3 +11,14 @@ def test_haproxy_config_file(host):
 
     assert f.exists
     assert f.contains('server  vm_tango_02  127.0.0.1:8080 check')
+
+
+def test_haproxy_is_installed(host):
+    haproxy = host.package("haproxy")
+    assert haproxy.is_installed  
+    assert haproxy.version.startswith("1.4")
+
+def test_haproxy_running_and_enabled(host):
+    haproxy = host.package("haproxy")
+    assert haproxy.is_running
+    assert haproxy.is_enabled
